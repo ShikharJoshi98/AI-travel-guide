d5977364eedbf562f9c5dcd407c11316e18576f4
ShikharJoshi98 <shikharjoshi89@gmail.com>
2024-12-21 06:26:34 +0530

    updated

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -49,9 +49,9 @@ function Header() {
       <div>
         {user ?
           <div className='flex items-center gap-3'>
-            <Link to={"/create-trip"}>
+            <a href='/create-trip'>
               <Button variant="outline" className="rounded-full">+ Create Trip</Button>
-            </Link>
+            </a>
             <a href="/my-trips">
               <Button variant="outline" className="rounded-full">My Trips</Button>
               </a>
